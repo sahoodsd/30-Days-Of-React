5142a944e8c89d3b9ddce9a0aec9d6704ed4db3a
sahoodsd <75218290+sahoodsd@users.noreply.github.com>
2025-09-15 20:35:18 +0400

    push

--- 30-days-of-react/src/index.js
+++ 30-days-of-react/src/index.js
@@ -2,71 +2,30 @@ import React, { Component } from "react";
 import ReactDOM from "react-dom/client";
 
 class App extends Component {
-  firstName = React.createRef();
-  lastName = React.createRef();
-  country = React.createRef();
-  title = React.createRef();
-
-  handleSumbit = (e) => {
-    e.preventDefault();
-    console.log(this.firstName.current.value);
-    console.log(this.lastName.current.value);
-    console.log(this.title.current.value);
-    console.log(this.country.current.value);
-
-    const data = {
-      firstName: this.firstName.current.value,
-      lastName: this.lastName.current.value,
-      title: this.title.current.value,
-      country: this.country.current.value,
+  constructor(props) {
+    super(props);
+    console.log("I am the constructor and i will be th frist one run.");
+    this.state = {
+      firstName: "",
     };
-    // the is the place we connect backend api to send the data to the database
-    console.log(data);
-  };
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log(
+      "I am getDerivedStateFromProps and I will be the second to run."
+    );
+    return null;
+  }
+
+  componentDidMount() {
+    console.log("I am componentDidMount and I will be last to run.");
+  }
 
   render() {
+    console.log("I am render and I will be the third to run.");
     return (
       <div className="App">
-        <h3>Add Student</h3>
-        <form onSubmit={this.handleSumbit}>
-          <div>
-            <input
-              type='text'
-              name='firstName'
-              placeholder='First Name'
-              ref={this.firstName}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              name='lastName'
-              placeholder='Last Name'
-              ref={this.lastName}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              name='country'
-              placeholder='Country'
-              ref={this.country}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <input
-              type='text'
-              name='title'
-              placeholder='Title'
-              ref={this.title}
-              onChange={this.handleChange}
-            />
-          </div>
-          <button className='btn btn-success'>Submit</button>
-        </form>
+        <h1>React Component Life Cycle</h1>
       </div>
     );
   }
